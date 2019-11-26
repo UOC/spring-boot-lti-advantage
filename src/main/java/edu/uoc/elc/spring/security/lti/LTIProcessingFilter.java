207f2fbab07d9c1f8434fa6bb1db9a45bb012f4d
Xavi Aracil <xaracil@uoc.edu>
2019-11-26 11:28:08 +0100

    Added reason to log

--- src/main/java/edu/uoc/elc/spring/security/lti/LTIProcessingFilter.java
+++ src/main/java/edu/uoc/elc/spring/security/lti/LTIProcessingFilter.java
@@ -51,7 +51,7 @@ public class LTIProcessingFilter extends AbstractPreAuthenticatedProcessingFilte
 			this.logger.info("Valid LTI call from " + tool.getUser().getId());
 			return tool.getUser().getId() + "-" + tool.getContext().getId();
 		}
-		this.logger.info("The request is not a valid LTI one");
+		this.logger.info("The request is not a valid LTI one. Reason: " + tool.getReason());
 		return null;
 	}
 
