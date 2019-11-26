cfb5bae4aa88c6493f47433b7b5ab1c715235788
Xavi Aracil <xaracil@uoc.edu>
2019-11-26 15:51:30 +0100

    Protection against null value

--- src/main/java/edu/uoc/elc/spring/security/lti/Context.java
+++ src/main/java/edu/uoc/elc/spring/security/lti/Context.java
@@ -3,6 +3,8 @@ package edu.uoc.elc.spring.security.lti;
 import edu.uoc.elc.lti.tool.Tool;
 import lombok.Getter;
 
+import java.util.List;
+
 /**
  * Represents a Course from LTI context data
  *
@@ -19,16 +21,20 @@ public class Context {
 		this.ltiContext = tool.getContext();
 	}
 
-	public String getName() {
-		return ltiContext.getLabel();
+	public String getId() {
+		return this.ltiContext != null ? this.ltiContext.getId() : null;
 	}
 
-	public String getKey() {
-		return ltiContext.getId();
+	public String getLabel() {
+		return this.ltiContext != null ? this.ltiContext.getLabel() : null;
 	}
 
 	public String getTitle() {
-		return ltiContext.getTitle();
+		return this.ltiContext != null ? this.ltiContext.getTitle() : null;
+	}
+
+	public List<String> getType() {
+		return this.ltiContext != null ? this.ltiContext.getType() : null;
 	}
 
 	public Object getCustomParameter(String name) {
