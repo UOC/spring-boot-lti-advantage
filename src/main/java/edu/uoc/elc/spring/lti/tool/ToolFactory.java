058508bfb22ad53a3b6a0e066b3cebe00e20c5a2
Xavi Aracil <xaracil@uoc.edu>
2019-12-05 16:27:42 +0100

    OIDC Session as a readable attribute

--- src/main/java/edu/uoc/elc/spring/lti/tool/ToolFactory.java
+++ src/main/java/edu/uoc/elc/spring/lti/tool/ToolFactory.java
@@ -4,6 +4,7 @@ import edu.uoc.elc.lti.tool.Tool;
 import edu.uoc.elc.lti.tool.ToolDefinition;
 import edu.uoc.elc.spring.lti.security.openid.HttpSessionOIDCLaunchSession;
 import edu.uoc.elc.spring.lti.security.utils.TokenFactory;
+import lombok.Getter;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,8 +12,11 @@ import javax.servlet.http.HttpServletRequest;
  * @author xaracil@uoc.edu
  */
 public class ToolFactory {
+	@Getter
+	private HttpSessionOIDCLaunchSession oidcLaunchSession;
+
 	public Tool from(ToolDefinitionBean toolDefinitionBean, HttpServletRequest request) {
-		final HttpSessionOIDCLaunchSession oidcLaunchSession = new HttpSessionOIDCLaunchSession(request);
+		this.oidcLaunchSession = new HttpSessionOIDCLaunchSession(request);
 		final ToolDefinition toolDefinition = ToolDefinitionFactory.from(toolDefinitionBean);
 		Tool tool = new Tool(toolDefinition,
 						toolDefinitionBean.getClaimAccessor(),
