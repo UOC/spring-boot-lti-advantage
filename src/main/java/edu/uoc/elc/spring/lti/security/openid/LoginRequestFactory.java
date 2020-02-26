3d038c632ee99aad7a5c45e3d16035076d7b92b5
heutelbeck <dheutelbeck@ftk.de>
2020-02-26 20:34:28 +0100

    cleanup of Lombok usage, logging and POM warnings

--- src/main/java/edu/uoc/elc/spring/lti/security/openid/LoginRequestFactory.java
+++ src/main/java/edu/uoc/elc/spring/lti/security/openid/LoginRequestFactory.java
@@ -1,21 +1,20 @@
 package edu.uoc.elc.spring.lti.security.openid;
 
-import edu.uoc.elc.lti.tool.oidc.LoginRequest;
-
 import javax.servlet.http.HttpServletRequest;
+
+import edu.uoc.elc.lti.tool.oidc.LoginRequest;
+import lombok.experimental.UtilityClass;
 
 /**
  * @author xaracil@uoc.edu
  */
+@UtilityClass
 public class LoginRequestFactory {
-	public static LoginRequest from(HttpServletRequest request) {
-		return LoginRequest.builder()
-						.iss(request.getParameter("iss"))
-						.login_hint(request.getParameter("login_hint"))
-						.target_link_uri(request.getParameter("target_link_uri"))
-						.lti_message_hint(request.getParameter("lti_message_hint"))
-						.lti_deployment_id(request.getParameter("lti_deployment_id"))
-						.client_id(request.getParameter("client_id"))
-						.build();
+	public LoginRequest from(HttpServletRequest request) {
+		return LoginRequest.builder().iss(request.getParameter("iss")).login_hint(request.getParameter("login_hint"))
+				.target_link_uri(request.getParameter("target_link_uri"))
+				.lti_message_hint(request.getParameter("lti_message_hint"))
+				.lti_deployment_id(request.getParameter("lti_deployment_id"))
+				.client_id(request.getParameter("client_id")).build();
 	}
 }
--- src/main/java/edu/uoc/elc/spring/lti/security/utils/TokenFactory.java
+++ src/main/java/edu/uoc/elc/spring/lti/security/utils/TokenFactory.java
@@ -2,11 +2,14 @@ package edu.uoc.elc.spring.lti.security.utils;
 
 import javax.servlet.http.HttpServletRequest;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * @author xaracil@uoc.edu
  */
+@UtilityClass
 public class TokenFactory {
-	public static String from(HttpServletRequest httpServletRequest) {
+	public String from(HttpServletRequest httpServletRequest) {
 		String token = httpServletRequest.getParameter("jwt");
 		if (token == null || "".equals(token)) {
 			token = httpServletRequest.getParameter("id_token");
