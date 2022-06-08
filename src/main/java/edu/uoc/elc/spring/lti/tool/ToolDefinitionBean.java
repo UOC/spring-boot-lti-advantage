dde020dd968a346bfe3a077b5b1f72509c54bea7
Xavi Aracil <xaracil@uoc.edu>
2022-06-08 15:22:18 +0200

    get RequestAwareOIDCLaunchSession from factory, so each bean get its own instance

--- src/main/java/edu/uoc/elc/spring/lti/tool/ToolDefinitionBean.java
+++ src/main/java/edu/uoc/elc/spring/lti/tool/ToolDefinitionBean.java
@@ -3,6 +3,7 @@ package edu.uoc.elc.spring.lti.tool;
 import edu.uoc.elc.lti.tool.Registration;
 import edu.uoc.elc.lti.tool.ToolBuilders;
 import edu.uoc.elc.spring.lti.security.openid.RequestAwareOIDCLaunchSession;
+import edu.uoc.elc.spring.lti.security.openid.RequestAwareOIDCLaunchSessionBeanFactory;
 import edu.uoc.elc.spring.lti.tool.builders.ClaimAccessorService;
 import edu.uoc.elc.spring.lti.tool.builders.ClientCredentialsTokenBuilderService;
 import edu.uoc.elc.spring.lti.tool.builders.DeepLinkingTokenBuilderService;
@@ -10,6 +11,8 @@ import edu.uoc.lti.accesstoken.AccessTokenRequestBuilder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * @author xaracil@uoc.edu
@@ -19,16 +22,23 @@ import org.springframework.stereotype.Component;
 public class ToolDefinitionBean {
 	@Getter
 	private final ClaimAccessorService claimAccessor;
-	@Getter
-	private final RequestAwareOIDCLaunchSession requestAwareOIDCLaunchSession;
 
 	private final DeepLinkingTokenBuilderService deepLinkingTokenBuilder;
 	private final ClientCredentialsTokenBuilderService clientCredentialsTokenBuilder;
 	private final AccessTokenRequestBuilder accessTokenRequestBuilder;
 
+	@Resource(name = "&requestAwareOIDCLaunchSession")
+	private RequestAwareOIDCLaunchSessionBeanFactory factory;
+
 	public ToolBuilders getBuilders(Registration registration, String kid) {
 		return new ToolBuilders(clientCredentialsTokenBuilder.getClientCredentialsTokenBuilder(registration, kid),
 						accessTokenRequestBuilder,
 						deepLinkingTokenBuilder.getDeepLinkingTokenBuilder(registration, kid));
+	}
+
+	public RequestAwareOIDCLaunchSession getRequestAwareOIDCLaunchSession() {
+		// gotten requestAwareOIDCLaunchSession through factory for allowing prototype scope,
+		// so we can create a new RequestAwareOIDCLaunchSession for each request
+		return factory.getObject();
 	}
 }
